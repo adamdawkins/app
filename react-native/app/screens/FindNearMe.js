f9f7d860c7392917ea46c04c868541cc5cd832a8
Adam Dawkins <adam@dragondrop.uk>
2017-01-19 21:18:32 +0000

    Lesson 1: Geolocation

--- react-native/app/screens/FindNearMe.js
+++ react-native/app/screens/FindNearMe.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
 import Container from '../components/Container';
 import { Header } from '../components/Text';
+import LocateMeButton from '../components/LocateMeButton';
 
 class FindNearMe extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handlePress = this.handlePress.bind(this);
+  }
   static route = {
     navigationBar: {
       visible: false,
     },
   }
+  handleGeolocationSuccess(position) {
+    const { coords } = position;
+    console.log('latitude', coords.latitude);
+    console.log('longitude', coords.longitude);
+
+  }
+
+  handleGeolocationError(error) {
+    console.error(error.message);
+  }
+
+  handlePress() {
+    navigator.geolocation.getCurrentPosition(
+      this.handleGeolocationSuccess,
+      this.handleGeolocationError,
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+  }
 
   render() {
     return (
       <Container>
+      <LocateMeButton onPress={this.handlePress} />
         <Header>
-          Find Near Me
+          Find Nearest Charging Stations
         </Header>
       </Container>
     );
